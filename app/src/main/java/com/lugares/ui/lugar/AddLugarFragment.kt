7f2f6f9a2b00e1501d31fe4acb1fd275ef5e2dde
Steven <sporras@ufide.ac.cr>
2022-08-03 16:56:33 -0600

    Semana 9 Almacenamiento de audios y fotos en firestore

--- app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
@@ -1,16 +1,30 @@
 package com.lugares.ui.lugar
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 import com.lugares.R
 import com.lugares.databinding.FragmentAddLugarBinding
 import com.lugares.model.Lugar
+import com.lugares.utiles.AudioUtiles
+import com.lugares.utiles.ImagenUtiles
 import com.lugares.viewmodel.LugarViewModel
 
 class AddLugarFragment : Fragment() {
@@ -19,6 +33,11 @@ class AddLugarFragment : Fragment() {
     private var _binding: FragmentAddLugarBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var audioUtiles: AudioUtiles
+    private lateinit var imagenUtiles: ImagenUtiles
+    private lateinit var tomarFotoActivity: ActivityResultLauncher<Intent>
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -26,12 +45,93 @@ class AddLugarFragment : Fragment() {
         lugarViewModel = ViewModelProvider(this)[LugarViewModel::class.java]
         _binding = FragmentAddLugarBinding.inflate(inflater,container,false)
         //Se crea la funcion para agregar lugares
-        binding.btAgregar.setOnClickListener { addLugar() }
+        binding.btAgregar.setOnClickListener {
+            binding.progressBar.visibility = ProgressBar.VISIBLE
+            binding.msgMensaje.text = getString(R.string.msg_subiendo_audio)
+            binding.msgMensaje.visibility = TextView.VISIBLE
+            subeAudio()
+        }
+        
+
+        audioUtiles = AudioUtiles(requireActivity(),
+            requireContext(),
+            binding.btAccion,
+            binding.btPlay,
+            binding.btDelete,
+            getString(R.string.msg_graba_audio),
+            getString(R.string.msg_detener_audio))
+
+        tomarFotoActivity= registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()) {
+            result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                imagenUtiles.actualizaFoto()
+            }
+        }
+
+        imagenUtiles = ImagenUtiles(requireContext(),
+        binding.btPhoto,
+        binding.btRotaL,
+        binding.btRotaR,
+        binding.imagen,
+            tomarFotoActivity
+        )
 
         return binding.root
     }
 
-    private fun addLugar() {
+    private fun subeAudio() {
+
+            val audioFile = audioUtiles.audioFile
+            if (audioFile.exists() && audioFile.isFile && audioFile.canRead()) {
+                val ruta = Uri.fromFile(audioFile)
+                val rutaNube =
+                    "lugaresApp/${Firebase.auth.currentUser?.email}/audios/${audioFile.name}"
+                val referencia: StorageReference = Firebase.storage.reference.child(rutaNube)
+                referencia.putFile(ruta)
+                    .addOnSuccessListener {
+                        referencia.downloadUrl
+                            .addOnSuccessListener {
+                                val rutaAudio = it.toString()
+                                subeImagen(rutaAudio)
+                            }
+                    }
+                    .addOnFailureListener {
+                        //Error al grabar audio en la nube...
+                        subeImagen("")
+                    }
+            } else { //Por alguna razón no hay archivo de audio...
+                subeImagen("")
+            }
+
+    }
+    private fun subeImagen(rutaAudio: String) {
+        binding.msgMensaje.text = getString(R.string.msg_subiendo_imagen)
+
+            val imagenFile = imagenUtiles.imagenFile
+            if (imagenFile.exists() && imagenFile.isFile && imagenFile.canRead()) {
+                val ruta = Uri.fromFile(imagenFile)
+                val rutaNube =
+                    "lugaresApp/${Firebase.auth.currentUser?.email}/imagenes/${imagenFile.name}"
+                val referencia: StorageReference = Firebase.storage.reference.child(rutaNube)
+                referencia.putFile(ruta)
+                    .addOnSuccessListener {
+                        referencia.downloadUrl
+                            .addOnSuccessListener {
+                                val rutaImagen = it.toString()
+                                addLugar(rutaAudio, rutaImagen)
+                            }
+                    }
+                    .addOnFailureListener {
+                        addLugar(rutaAudio, "")
+                    }
+            } else {
+                addLugar(rutaAudio, "")
+            }
+
+    }
+
+    private fun addLugar(rutaAudio: String, rutaImagen: String) {
         val nombre = binding.etNombre.text.toString()
         val correo = binding.etCorreo.text.toString()
         val telefono = binding.etTelefono.text.toString()
