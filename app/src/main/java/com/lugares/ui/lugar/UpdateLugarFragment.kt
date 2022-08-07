922d02fdd617d7b2b55e0db953c0763e07596db1
Steven <sporras@ufide.ac.cr>
2022-08-07 13:52:00 -0600

    Semana 12

--- app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -69,7 +69,7 @@ class UpdateLugarFragment : Fragment() {
         } else {
             binding.btPlay.isEnabled=false
         }
-        //Si hay ruta de imagen...se dibuja
+        //Si hay ruta de imagen...entonces se dibuja
         if (args.lugar.rutaImagen?.isNotEmpty()==true){
             Glide.with(requireContext())
                 .load(args.lugar.rutaImagen)
